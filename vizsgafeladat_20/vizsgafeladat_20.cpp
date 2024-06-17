d72f856a55518354cb5b635e1c22df017d445681
cybermayer <154334430+cybermayer@users.noreply.github.com>
2024-06-17 22:14:13 +0200

    Create vizsgafeladat_20.cpp

---
+++ vizsgafeladat_20/vizsgafeladat_20.cpp
@@ -0,0 +1,13 @@
+/*
+
+Definiálja azt a void rotateLines(const std::string& fileName) függvényt, 
+ami a fileName nevű fájlban található szövegsorokat egymás után kiolvassa, 
+majd megjeleníti őket eredeti alakjukban, illetve 1, 2, ..., n-1 karakterrel balra forgatva is, 
+ahol n a karakterlánc hossza.
+  
+Tájékoztatásul, a words1.txt fájlt tartalma a következő:
+
+abcdef
+qwertzuiop
+
+*/
